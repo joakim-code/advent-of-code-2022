a0226709a34026077e51777240c4845e5d9f1c10
Joakim <joakim.andre@gmail.com>
2022-12-10 19:14:10 +0100

    Day06

--- src/Day06/Program.cs
+++ src/Day06/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = System.IO.File.ReadAllLines(@"input.txt");
+
+foreach(string line in input) {
+
+    int partOneMarker = findMarker(line, 4);
+    Console.WriteLine("Answer Part One - Marker pos: {0}", partOneMarker);
+
+    int partTwoMarker = findMarker(line, 14);
+    Console.WriteLine("Answer Part Two - Marker pos: {0}", partTwoMarker);
+
+}
+
+int findMarker(string datastream, int markerLength) {
+    
+    int markerPos = 0;
+    for(int i=0;i<datastream.Length;i++) {
+        if(isMarker(datastream.Substring(i,markerLength))) {
+            markerPos = i + markerLength;
+            break;
+        }
+    }
+
+    return markerPos;
+}
+
+bool isMarker(string data) {
+
+    int count = 0;
+    
+    foreach(char i in data) {    
+        foreach(char j in data) {
+            if(i.Equals(j))
+                count ++;   
+        }
+    }
+
+    if(count > data.Length) {
+        return false;
+    } else {
+        return true;
+    }
+
+}
